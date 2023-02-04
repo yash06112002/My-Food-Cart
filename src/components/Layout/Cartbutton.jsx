2dbe9a37bdcc80ccff342f9af13f3d8c0b8e2a9d
Yash Kansara <82724814+yash06112002@users.noreply.github.com>
2023-02-04 19:02:31 +0530

    Cart button highlight

--- src/components/Layout/Cartbutton.jsx
+++ src/components/Layout/Cartbutton.jsx
@@ -1,15 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import CartIcon from "../Cart/CartIcon";
 import classes from "./Cartbutton.module.css";
 import CartContext from '../../store/CartContext';
 
 const Cartbutton = (props) => {
+    const [highlightBtn, setHighlightBtn] = useState(false);
     const cartCtx = useContext(CartContext);
     const numOfItems = cartCtx.items.reduce((currNum, item) => {
         return currNum + item.amount
     }, 0)
+
+    const btnClass = `${classes.button} ${highlightBtn ? classes.bump : ""}`
+    useEffect(() => {
+        if (cartCtx.items.length === 0) return
+
+        setHighlightBtn(true);
+        const timer = setTimeout(() => {
+            setHighlightBtn(false)
+        }, 300);
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [cartCtx.items])
     return (
-        <button className={classes.button} onClick={props.showCartHandler}>
+        <button className={btnClass} onClick={props.showCartHandler}>
             <span className={classes.icon}><CartIcon /></span>
             <span>Your Cart</span>
             <span className={classes.badge}>{numOfItems}</span>
